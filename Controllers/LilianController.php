300de3b7892fe3818109a5468e7a71bb3a712919
lilian.corbioli <corbioli.lilian@gmail.com>
2022-11-07 10:03:06 +0100

    feat(dev): Add new page

--- Controllers/LilianController.php
+++ Controllers/LilianController.php
@@ -1,11 +1,32 @@
 <?php
 
 namespace App\Controllers;
+use App\Entity\Artist;
 
 class LilianController extends Controller
 {
     public function index()
     {
-        $this->render('lilian/index');
-    }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://api.spotify.com/v1/search?q=orelsan&type=artist");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $_SESSION['token']));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $json_obj = json_decode($result);
+        $listArtists = array();
+        foreach ($json_obj->{"artists"}->{'items'} as $item) {
+            $img = (!empty($item->images)) ? $item->images[0]->{'url'} : "" ;
+            $artist = new Artist(
+                $item->{'id'},
+                $item->{'name'},
+                $item->{'followers'}->{'total'},
+                $item->genres,
+                $item->{'external_urls'}->{'spotify'},
+                $img
+            );
+            array_push($listArtists,$artist);
+        }
+        $this->render('lilian/index', compact('listArtists'));
+        }
 }
--- View/lilian/index.php
+++ View/lilian/index.php
@@ -1,5 +1,36 @@
 <?php
 use App\Autoloader;
 use App\Entity\Artist;
+Autoloader::register();
+
 ?>
-<h1>Page de lilian</h1>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>Ma liste d'artiste</title>
+</head>
+<body>
+<div class="row">
+    <?php
+    foreach ($listArtists as $artist) {
+        //var_dump($artist);
+        echo '        
+        <div Entity="card" style="width: 18rem">
+            <img src='.$artist->getPicture().' class="card-img-top">
+            <div Entity="card-body">
+                <h5 Entity="card-title">'.$artist->getName().'</h5>
+                <p Entity="card-text">nombre de follower: <strong>'.$artist->getFollowers().'</strong></p>
+                <a href='.$artist->getLink().'>Lien vers Spotify</a>
+            </div>
+        </div>';
+    }
+        ?>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
